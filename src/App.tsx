7fb19aace3c477a5f4afdfbd2b056a3219b99344
Zamir <eazzy_freez@mail.ru>
2023-04-02 16:07:29 +0600

    add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,34 @@
-import React, { useEffect } from 'react';
-import { auth } from './firebase';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectUser, login, logout } from './features/userSlice';
-import Login from './pages/Login/Login';
-import Main from './pages/Main/Main';
+import React, { useEffect } from "react";
+import { auth } from "./firebase";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
+import "./index.css";
 
 function App() {
-  const user = useSelector(selectUser);
-  const dispatch = useDispatch();
+	const user = useSelector(selectUser);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
-      if(authUser) {
-        //login
-        dispatch(
-          login({
-            uid: authUser.uid,
-            photo: authUser.photoURL,
-            email: authUser.email,
-            displayName: authUser.displayName
-          })
-        )
-      } else {
-        dispatch(logout())
-      }
-    })
-  }, [dispatch]);
+	useEffect(() => {
+		auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				//login
+				dispatch(
+					login({
+						uid: authUser.uid,
+						photo: authUser.photoURL,
+						email: authUser.email,
+						displayName: authUser.displayName,
+					}),
+				);
+			} else {
+				dispatch(logout());
+			}
+		});
+	}, [dispatch]);
 
-  return (
-    <div className="App">
-      {user ? <Main /> : <Login />}
-    </div>
-  );
+	return <div className="App">{user ? <Main /> : <Login />}</div>;
 }
 
 export default App;
