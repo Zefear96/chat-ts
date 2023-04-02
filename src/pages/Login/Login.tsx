b38bbe6c7e1101229d42e3800dd648cb4aa9974c
Zamir <eazzy_freez@mail.ru>
2023-04-02 20:58:55 +0600

    add auth

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -15,6 +15,19 @@ import db from "../../firebase";
 import { doc, setDoc } from "firebase/firestore";
 import SignUpWithEmail from "./SignUpWithEmail";
 import SignInWithEmail from "./SignInWithEmail";
+import SignInWithPhone from "./SignInWithPhone";
+
+import {
+	Tabs,
+	Tab,
+	Container,
+	Box,
+	Typography,
+	TextField,
+	Checkbox,
+	// Button,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
 	const [signUp, setSignUp] = useState<number>(0);
@@ -81,23 +94,28 @@ const Login = () => {
 			});
 	};
 
+	const [activeTab, setActiveTab] = useState(0);
+
+	const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+		setActiveTab(newValue);
+	};
+
+	const navigate = useNavigate();
 	return (
 		<Wrapper>
 			<LoginTg>
-				<div>
-					{/* <img src={img} alt='logo' /> */}
-					{/* <h1>Welcome!</h1> */}
-				</div>
 				{signUp === 0 ? null : signUp === 1 ? (
 					<SignUpWithEmail signUpWithEmail={signUpWithEmail} />
 				) : signUp === 2 ? (
 					<SignInWithEmail signInWithEmail={signInWithEmail} />
 				) : null}
 			</LoginTg>
+
 			<Buttons>
 				{signUp === 0 ? (
 					<>
 						<Button onClick={() => setSignUp(1)}>Sign Up</Button>
+						<Button onClick={() => setSignUp(0)}>Sign Phone</Button>
 						<Button onClick={() => setSignUp(2)}>
 							Sign In With Email
 							<EmailIcon />
@@ -115,12 +133,115 @@ const Login = () => {
 					<span>Sign In With Google</span>
 					<GoogleIcon />
 				</Button>
-				<Button onClick={signIn}>
+				<Button onClick={() => navigate("/signphone")}>
 					<span>Sign In With Phone Number</span>
 					<PermPhoneMsgIcon />
 				</Button>
 			</Buttons>
 		</Wrapper>
+
+		// <Container maxWidth="sm" sx={{ mt: 3 }}>
+		// 	<Box sx={{ borderBottom: 1, borderColor: "divider", mb: 3 }}>
+		// 		<Tabs
+		// 			value={activeTab}
+		// 			onChange={handleTabChange}
+		// 			centered
+		// 			indicatorColor="primary"
+		// 			textColor="primary"
+		// 		>
+		// 			<Tab label="Login" onClick={() => navigate("/login")} />
+		// 			<Tab label="Register" onClick={() => navigate("/register")} />
+		// 		</Tabs>
+		// 	</Box>
+		// 	{activeTab === 0 && (
+		// 		<Box component="form" sx={{ mb: 3 }}>
+		// 			<TextField
+		// 				label="Email address"
+		// 				variant="outlined"
+		// 				fullWidth
+		// 				margin="normal"
+		// 				autoFocus
+		// 				required
+		// 			/>
+		// 			<TextField
+		// 				label="Password"
+		// 				variant="outlined"
+		// 				fullWidth
+		// 				margin="normal"
+		// 				type="password"
+		// 				required
+		// 			/>
+		// 			<Box sx={{ display: "flex", alignItems: "center", mb: 2 }}>
+		// 				<Checkbox defaultChecked />
+		// 				<Typography variant="body2">Remember me</Typography>
+		// 			</Box>
+		// 			<Button variant="contained" fullWidth>
+		// 				Sign in
+		// 			</Button>
+		// 			<Button
+		// 				variant="contained"
+		// 				fullWidth
+		// 				onClick={signIn}
+		// 				color="success"
+		// 			>
+		// 				Sign in with Google
+		// 			</Button>
+
+		// 			<Button variant="contained" fullWidth onClick={signIn}>
+		// 				Sign in with Phone Number
+		// 			</Button>
+
+		// 			<Typography variant="body2" sx={{ mt: 2, textAlign: "center" }}>
+		// 				Not a member?{" "}
+		// 				<a
+		// 					onClick={() => navigate("/register")}
+		// 					style={{ cursor: "pointer", textDecoration: "underline" }}
+		// 				>
+		// 					Register
+		// 				</a>
+		// 			</Typography>
+		// 		</Box>
+		// 	)}
+		// 	{activeTab === 1 && (
+		// 		<Box component="form" sx={{ mb: 3 }}>
+		// 			<Typography variant="h6" sx={{ mb: 3 }}>
+		// 				Sign up with:
+		// 			</Typography>
+		// 			<TextField
+		// 				label="Username"
+		// 				variant="outlined"
+		// 				fullWidth
+		// 				margin="normal"
+		// 				autoFocus
+		// 				required
+		// 			/>
+		// 			<TextField
+		// 				label="Email"
+		// 				variant="outlined"
+		// 				fullWidth
+		// 				margin="normal"
+		// 				required
+		// 			/>
+		// 			<TextField
+		// 				label="Password"
+		// 				variant="outlined"
+		// 				fullWidth
+		// 				margin="normal"
+		// 				type="password"
+		// 				required
+		// 			/>
+		// 			<Box sx={{ display: "flex", alignItems: "center", mb: 2 }}>
+		// 				<Checkbox defaultChecked />
+		// 				<Typography variant="body2">
+		// 					I have read and agree to the terms
+		// 				</Typography>
+		// 			</Box>
+		// 			<Button variant="contained" fullWidth onClick={() => setSignUp(1)}>
+		// 				Sign up
+		// 			</Button>
+		// 		</Box>
+		// 	)}
+		// </Container>
 	);
 };
 
